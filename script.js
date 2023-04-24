376b18b7a44a8978deefd7466f18ec00023264f0
dallen1 <dallen1@epscor.unm.edu>
2023-04-24 09:11:52 -0600

    1a

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-//Question 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+console.log(`----------------
+Question 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+----------------`);
 
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
+console.log(ages);
+
+console.log(`----------------
+1a. Programmatically subtract the value of the first element in the array from the value in the last element of the array 
+    •	Do not use numbers to reference the last element, find it programmatically, 
+    •	ages[7] – ages[0] is not allowed!
+----------------`);
+
+console.log(ages[ages.length -1] - ages[0]);
