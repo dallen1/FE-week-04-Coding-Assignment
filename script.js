083582ee8c6a1a6cc0d953ec79519416af26a894
dallen1 <dallen1@epscor.unm.edu>
2023-04-24 09:13:42 -0600

    added comments

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log(`----------------
 Question 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
 ----------------`);
-
+//initializing the array scoped to the block level with let
 let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
 console.log(ages);
@@ -12,4 +12,6 @@ console.log(`----------------
     •	ages[7] – ages[0] is not allowed!
 ----------------`);
 
+//uses the length attribute to programtically get the last index
 console.log(ages[ages.length -1] - ages[0]);
+
